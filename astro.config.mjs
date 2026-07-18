b55ba2dc44ba9f3a4c85ae1d7d72724a6034c734
Arukast <altatubagus@gmail.com>
2026-07-18 13:32:22 +0700

    chore: configure site and base paths for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://labeim.github.io',
+  base: '/website-eim',
   vite: {
     plugins: [tailwindcss()],
     resolve: {
